7ee1cf6c516f63e20a6b4a3621d31e49e74c411d
Michael Pinkham <mpinkham3@gmail.com>
2017-04-15 17:47:18 -0400

    Add error checking, argparse, update to match spec

--- ngram.py
+++ ngram.py
@@ -1,32 +1,90 @@
 #!/usr/bin/env python3
 
 import argparse
-import itertools as it
+import collections
+import sys
 
-def stream_ngram(file_, chunksize=8192):
+#TODO: write documentation (use google format)
+#TODO: reorder parameters
+def stream_ngram(file_, n=3, slide=1, chunksize=8190):
+  if n < 1 or n > 3:
+    #TODO:Throw invalid n
+    raise Exception('Bad value.', 'Unsupported n for ngram')
+
+  if slide < 1 or slide > n:
+    #TODO:Throw invalid slide
+    raise Exception('Bad value.', 'Unsupported slide for ngram')
+
+  #TODO: check default exception on file open fail
   with open(file_, "rb") as f:
-    trigram = None
+    remaining_bytes = None
+    #trigram = None
     while True:
       chunk = f.read(chunksize)
       if chunk:
-        if trigram != None:
+        #if trigram != None:
+        if remaining_bytes:
           # Account for ngrams split across the chunksize
-          split_tri = trigram[1:] + bytes(chunk[0:1])
-          yield (split_tri[:1], split_tri[:2], split_tri)
-          split_tri = trigram[2:3] + bytes(chunk[:1])
-          yield (split_tri[:1], split_tri[:2], split_tri)
-        for i in range(chunksize-2):
-          trigram = chunk[i:i+3]
-          bigram  = trigram[:2]
-          unigram = trigram[:1]
-          yield (unigram, bigram, trigram)
+          if n == 2:
+            assert(len(remaining_bytes)==1)
+            split_bi = remaining_bytes + bytes(chunk[0:1])
+            if len(split_bi) == n:
+              yield split_bi
+          elif n == 3:
+            assert(len(remaining_bytes)==2)
+            split_tri = remaining_bytes + bytes(chunk[0:1])
+            if len(split_tri) == n:
+              yield split_tri
+            if chunk[1] and slide == 1:
+              split_tri = remaining_bytes[-1:] + bytes(chunk[0:2])
+              if len(split_tri) == n:
+                yield split_tri
+          
+        for i in range(0, chunksize-n+1, slide):
+          gram = chunk[i:i+n]
+          if len(gram) == n:
+            yield gram
+        remaining_bytes = chunk[i+slide:]
         
       else:
-        #the remaining bigram/uni and uni
-        yield (chunk[-2:-1], chunk[-2:], None) 
-        yield (chunk[-1:], None, None)
         break
 
+def handle(exception_inst):
+    print(exception_inst.args)
+    sys.exit(1)
+
+def count_ngrams(nbytes, slide, file_):
+  chunksize = 6
+  counts = collections.defaultdict(lambda: 0)
+  try:
+    for gram in stream_ngram('tmp.txt', nbytes, slide, chunksize):
+      counts[gram] += 1
+  except Exception as inst:
+    handle(inst)
+  return counts
+
+def output_sorted_counts(count_dict, file_):
+  #TODO:sort dict by counts
+  toptwenty = [(k, count_dict[k]) for k in sorted(count_dict,
+    key=count_dict.get, reverse=True)]
+  print(toptwenty)
+  return
+
+def main():
+  parser = argparse.ArgumentParser(description="Static binary analysis by"
+    "processing ngrams in the input where 1 <= n <= 3.")
+  parser.add_argument('-n', '--nbytes', type=int, 
+    help='The number of bytes in the ngram')
+  parser.add_argument('-s', '--slidelen', type=int,
+    help='The number of bytes between each ngram read from the file. s <= n.')
+  parser.add_argument('-in', '--infile', help='The name of the file to analyze')
+  parser.add_argument('-out', '--outfile', 
+    help='The name of the file to write program results to.')
+  args = parser.parse_args()
+
+  counts = count_ngrams(args.nbytes, args.slidelen, args.infile)
+  output_sorted_counts(counts, args.outfile)
+
+
 if __name__ == '__main__':
-  for tri in stream_ngram('starter.py', 5):
-    print(tri)
+  main()
