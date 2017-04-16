60310a7c47f7bb095b668e7b12a57cabfa20803a
Michael Pinkham <mpinkham3@gmail.com>
2017-04-16 16:24:36 +0000

    Add docstrings for ngram.py

--- ngram.py
+++ ngram.py
@@ -7,11 +7,39 @@ import sys
 
 #TODO: write documentation (use google format)
 def stream_ngram(file_, n=3, slide=1, chunksize=8190):
+  """
+  Given a binary file to read, a value of n, and a slide value, 
+  `stream_ngram` generates all the ngrams in the file, returning
+  an iterator over them.
+
+  Args:
+      file_     (unicode): A file name to open
+      n         (int):     The number of bytes to return each iteration
+      slide     (int):     The number of bytes to slide forward in the 
+                           file at each iteration. assert slide <= n
+      chunksize (int):     A number of bytes to read from the file at a 
+                           time. Since the program only supports 
+                           unigrams, bigrams, and trigrams, the chunksize
+                           should be divisible by 6. The program quits 
+                           otherwise.
+  
+  Yields:
+      (bytearray): the next n-byte bytearray
+  
+  Raises:
+      Mildly descriptive bad value exceptions for unsupported parameter
+      values, specifically an error will be thrown if n < 1, n > 3, slide < 1, 
+      slide > n, chunksize % 6 != 0, or the program cannot find or open the 
+      file.
+  """
   if n < 1 or n > 3:
     raise Exception('Bad value.', 'Unsupported n for ngram')
 
   if slide < 1 or slide > n:
     raise Exception('Bad value.', 'Unsupported slide for ngram')
+
+  if chunksize % 6:
+    raise Exception('Bad value.', 'Unsupported chunksize for file read.')
 
   try:
     with open(file_, "rb") as f:
@@ -52,10 +80,32 @@ def stream_ngram(file_, n=3, slide=1, chunksize=8190):
 
 
 def handle(exception_inst):
-    print(exception_inst.args)
-    sys.exit(1)
+  """
+  Print an error and exit.
+
+  Args:
+      exception_inst (Exception object): the Exception that was raised.
+
+  Returns:
+      (void) System exits with return value of 1 (error).
+  """
+  print(exception_inst.args)
+  sys.exit(1)
 
 def count_ngrams(nbytes, slide, file_):
+  """
+  Return a dictionary mapping bytearrays to counts.
+
+  Args:
+      nbytes (int): the size of the bytearray i.e. ngram
+      slide  (int): the size of the slide value
+      file_  (unicode): the name of the file to analyze
+
+  Returns:
+      (dict[bytearray]:int): a mapping of each ngram to the number of times
+                             the ngram appeared in the file (given the slide)
+                             Exits the program on failure.
+  """
   counts = collections.defaultdict(lambda: 0)
   try:
     for gram in stream_ngram(file_, nbytes, slide):
@@ -65,6 +115,16 @@ def count_ngrams(nbytes, slide, file_):
   return counts
 
 def output_sorted_counts(count_dict, file_):
+  """
+  Writes the count dictionary to a json file.
+
+  Args:
+      count_dict (dict[bytearray]:int): see `count_ngrams`
+      file_      (unicode)            : the name of the file to write to
+  
+  Returns:
+      (void)
+  """
   counts = [(k.hex(), count_dict[k]) for k in sorted(count_dict,
     key=count_dict.get, reverse=True)]
   with open(file_, 'w') as f:
@@ -72,6 +132,9 @@ def output_sorted_counts(count_dict, file_):
   return
 
 def main():
+  """
+  See the README. Run `$ ./ngram.py -h` to see options.
+  """
   parser = argparse.ArgumentParser(description="Static binary analysis by"
     "processing ngrams in the input where 1 <= n <= 3.")
   parser.add_argument('-n', '--nbytes', type=int, 
