2003ed6ba9c7f38d572443a7fda7d6af290717ae
Michael Pinkham <mpinkham3@gmail.com>
2017-04-16 13:37:42 +0000

    Actually output to json file this time; add README

---
+++ README.md
@@ -0,0 +1,22 @@
+Michael Pinkham
+UNI: mlp2185
+COMS W4180
+Programming Project 2
+README
+
+Part 1
+
+Install:
+#TODO
+
+Run: 
+ - `$ ./ngram.py -n1 -s1  -out path/to/output.json -in path/to/progN`
+ - These arguments can be in any order. Note that n is the n in ngram 
+and s is the slide.
+
+Output:
+ - This program outputs all ngrams and their counts given the parameters 
+in a JSON-format file. Specifically, the output file will contain a list
+of two-element lists of the form [ngram, count]. 
+
+
--- ngram.py
+++ ngram.py
@@ -2,72 +2,73 @@
 
 import argparse
 import collections
+import json
 import sys
 
 #TODO: write documentation (use google format)
-#TODO: reorder parameters
 def stream_ngram(file_, n=3, slide=1, chunksize=8190):
   if n < 1 or n > 3:
-    #TODO:Throw invalid n
     raise Exception('Bad value.', 'Unsupported n for ngram')
 
   if slide < 1 or slide > n:
-    #TODO:Throw invalid slide
     raise Exception('Bad value.', 'Unsupported slide for ngram')
 
-  #TODO: check default exception on file open fail
-  with open(file_, "rb") as f:
-    remaining_bytes = None
-    #trigram = None
-    while True:
-      chunk = f.read(chunksize)
-      if chunk:
-        #if trigram != None:
-        if remaining_bytes:
-          # Account for ngrams split across the chunksize
-          if n == 2:
-            assert(len(remaining_bytes)==1)
-            split_bi = remaining_bytes + bytes(chunk[0:1])
-            if len(split_bi) == n:
-              yield split_bi
-          elif n == 3:
-            assert(len(remaining_bytes)==2)
-            split_tri = remaining_bytes + bytes(chunk[0:1])
-            if len(split_tri) == n:
-              yield split_tri
-            if len(chunk) > 1 and slide == 1:
-              split_tri = remaining_bytes[-1:] + bytes(chunk[0:2])
+  try:
+    with open(file_, "rb") as f:
+      remaining_bytes = None
+      #trigram = None
+      while True:
+        chunk = f.read(chunksize)
+        if chunk:
+          #if trigram != None:
+          if remaining_bytes:
+            # Account for ngrams split across the chunksize
+            if n == 2:
+              assert(len(remaining_bytes)==1)
+              split_bi = remaining_bytes + bytes(chunk[0:1])
+              if len(split_bi) == n:
+                yield split_bi
+            elif n == 3:
+              assert(len(remaining_bytes)==2)
+              split_tri = remaining_bytes + bytes(chunk[0:1])
               if len(split_tri) == n:
                 yield split_tri
+              if len(chunk) > 1 and slide == 1:
+                split_tri = remaining_bytes[-1:] + bytes(chunk[0:2])
+                if len(split_tri) == n:
+                  yield split_tri
+            
+          for i in range(0, chunksize-n+1, slide):
+            gram = chunk[i:i+n]
+            if len(gram) == n:
+              yield gram
+          remaining_bytes = chunk[i+slide:]
           
-        for i in range(0, chunksize-n+1, slide):
-          gram = chunk[i:i+n]
-          if len(gram) == n:
-            yield gram
-        remaining_bytes = chunk[i+slide:]
-        
-      else:
-        break
+        else:
+          break
+    
+  except Exception as inst:
+    handle(inst)
+
 
 def handle(exception_inst):
     print(exception_inst.args)
     sys.exit(1)
 
 def count_ngrams(nbytes, slide, file_):
-  chunksize = 6
   counts = collections.defaultdict(lambda: 0)
-  #try:
-  for gram in stream_ngram(file_, nbytes, slide, chunksize):
-    counts[gram] += 1
-  #except Exception as inst:
-  #  handle(inst)
+  try:
+    for gram in stream_ngram(file_, nbytes, slide):
+      counts[gram] += 1
+  except Exception as inst:
+    handle(inst)
   return counts
 
 def output_sorted_counts(count_dict, file_):
-  #TODO:output to file
-  toptwenty = [(k, count_dict[k]) for k in sorted(count_dict,
-    key=count_dict.get, reverse=True)][:20]
-  print(toptwenty)
+  counts = [(k.hex(), count_dict[k]) for k in sorted(count_dict,
+    key=count_dict.get, reverse=True)]
+  with open(file_, 'w') as f:
+    json.dump(counts, f)
   return
 
 def main():
